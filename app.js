0bdcfea06454168586772a5b734c32ab8d64d895
Bryan Clayton <bclayton2k8@aol.com>
2018-10-30 20:42:34 -0500

    Made updates to complete part 1 of lab assignment

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 let friends = ['Tiffany','Joi','Jada','Shelby','Jason'];
 
-console.log(friends[0]);
+for (let i = 0; i < friends.length; i++) {
+    let friend = friends[i]; 
+    console.log(friend.toUpperCase() + ':' +  "\n\n");
+    for (let j = 99; j > 0; j--) {
+        let minusOne = j - 1; 
+            if (j > 1) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friend + " strikes one out, " + "clears it all out, " + j + " lines of code in the file. ")
+            } else {
+                console.log("1 line of code in the file, 1 line of code; " + friend + " strikes one out, clears it all out , no more lines of code in the file.")
+            }
+    }
+}
+
+
+
+
+        
+    
+
+  
